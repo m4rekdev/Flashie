221675f82b9fcf31045e6e5d35e071350787e2ce
goodbyepavlyi <pavlyi.off@gmail.com>
2022-09-18 15:08:15 +0200

    Platforms and OptionTypes enums

--- assets/enums.js
+++ assets/enums.js
@@ -7,5 +7,26 @@ module.exports = {
         CHAT_INPUT: 1,
         USER: 2,
         MESSAGE: 3,
+    },
+    OptionType: {
+        SUB_COMMAND: 1,
+        SUB_COMMAND_GROUP: 2,
+        STRING: 3,
+        INTEGER: 4,
+        BOOLEAN: 5,
+        USER: 6,
+        CHANNEL: 7,
+        ROLE: 8,
+        MENTIONABLE: 9,
+        NUMBER: 10,
+        ATTACHMENT: 11,
+    },
+    Platforms: {
+        discord: 'Discord',
+        minecraft: 'Minecraft',
+        instagram: 'Instagram',
+        tiktok: 'TikTok',
+        youtube: 'YouTube',
+        twitter: 'Twitter',
     }
 }
