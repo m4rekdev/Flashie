83fbebee1c03e45859bce26f403acc92d8b8e238
goodbyepavlyi <pavlyi.off@gmail.com>
2022-09-18 14:52:19 +0200

    error walk fix

--- utils/filesystem.js
+++ utils/filesystem.js
@@ -1,30 +1,19 @@
 const path = require('path');
 const { readdirSync, statSync } = require('fs');
 
-const walk = (dir) => {
+const walk = (directory) => {
     const results = [];
 
-    readdirSync(dir).forEach(dirItem => {
-        const stat = statSync(path.join(dir, dirItem));
+    readdirSync(directory).forEach(directoryItem => {
+        const stat = statSync(path.join(directory, directoryItem));
 
-        if (stat.isFile()) return results.push(path.join(dir, dirItem));
-        if (stat.isDirectory()) walk(path.join(dir, dirItem)).forEach(walkItem => results.push(walkItem));
+        if (stat.isFile()) return results.push(path.join(directory, directoryItem));
+        if (stat.isDirectory()) walk(path.join(directory, directoryItem)).forEach(walkItem => results.push(walkItem));
     });
 
     return results;
 };
 
 module.exports = {
-    walk: (directory) => {
-        const results = [];
-
-        readdirSync(directory).forEach(directoryItem => {
-            const stat = statSync(path.join(directory, directoryItem));
-    
-            if (stat.isFile()) return results.push(path.join(directory, directoryItem));
-            if (stat.isDirectory()) walk(path.join(directory, directoryItem)).forEach(walkItem => results.push(walkItem));
-        });
-    
-        return results;
-    },
+    walk,
 };
