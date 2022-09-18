a2086f2d3f6439437d228a43c95d0e29293095ef
goodbyepavlyi <pavlyi.off@gmail.com>
2022-09-18 14:30:31 +0200

    walk transformed into a arrow function

--- utils/filesystem.js
+++ utils/filesystem.js
@@ -1,19 +1,17 @@
 const path = require('path');
 const { readdirSync, statSync } = require('fs');
 
-function walk(dir) {
-    const results = [];
-
-    readdirSync(dir).forEach(dirItem => {
-        const stat = statSync(path.join(dir, dirItem));
-
-        if (stat.isFile()) return results.push(path.join(dir, dirItem));
-        if (stat.isDirectory()) walk(path.join(dir, dirItem)).forEach(walkItem => results.push(walkItem));
-    });
-
-    return results;
-};
-
 module.exports = {
-    walk,
+    walk: (directory) => {
+        const results = [];
+
+        readdirSync(directory).forEach(directoryItem => {
+            const stat = statSync(path.join(directory, directoryItem));
+    
+            if (stat.isFile()) return results.push(path.join(directory, directoryItem));
+            if (stat.isDirectory()) walk(path.join(directory, directoryItem)).forEach(walkItem => results.push(walkItem));
+        });
+    
+        return results;
+    },
 };
